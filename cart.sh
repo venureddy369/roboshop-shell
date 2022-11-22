9271df54cda505b935773ac5385223cac4c2322a
Venu <vennugopal.challa369@gmail.com>
2022-11-22 11:47:34 +0530

    cart Script

---
+++ cart.sh
@@ -0,0 +1,23 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+
+useradd roboshop
+
+
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+cd /home/roboshop
+rm -rf cart
+unzip -o /tmp/cart.zip
+mv cart-main cart
+cd cart
+npm install
+
+sed -i -e 's/REDIS_ENDPOINT/redis.devops69.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devops.online/' systemd.service
+
+
+
+
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+systemctl daemon-reload
+systemctl restart cart
+systemctl enable cart
