8ae39d0479d78ae2fe52e3e43e3e661cd5cd4438
Venu <vennugopal.challa369@gmail.com>
2022-11-26 09:23:53 +0530

    Mysql Script

--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,40 @@
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+    echo Failure
+fi
+
 dnf module disable mysql -y
+
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+    echo Failure
+fi
 
 yum install mysql-community-server -y
 
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+    echo Failure
+fi
+
 systemctl enable mysqld
+
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+    echo Failure
+fi
 systemctl start mysqld
+
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+    echo Failure
+fi
 
 echo show databases | mysql -uroot -pRoboshop@1   #-->if  this command giving output that means password changed
 if [ $? -ne 0 ]
