8caeab2033f394268abada950c4aef1ef3a735a0
Venu <vennugopal.challa369@gmail.com>
2022-11-26 09:16:32 +0530

    Mysql Script

--- mysql.sh
+++ mysql.sh
@@ -7,14 +7,16 @@ systemctl enable mysqld
 systemctl start mysqld
 
 echo show databases | mysql -uroot -pRoboshop@1   #-->if  this command giving output that means password changed
-
+if [ $? -ne 0]
+  then
 #resetting of password
 
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Roboshop@1';" >/tmp/root-pass-sql
-##(here above cmnd we have entered the password but that not ac ode standards always don't hard code the passwords)
+  echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Roboshop@1';" >/tmp/root-pass-sql
+  ##(here above cmnd we have entered the password but that not ac ode standards always don't hard code the passwords)
 
 
-DEFAULT_PASSWORD=$(grep 'A temporary password is generated' /var/log/mysqld.log | awk '{print $NF}')
+  DEFAULT_PASSWORD=$(grep 'A temporary password is generated' /var/log/mysqld.log | awk '{print $NF}')
 
 
-cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
+  cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
+fi
