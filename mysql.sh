9810eaa5dcb46a6bc400a331c0a6cff83523385c
Venu <vennugopal.challa369@gmail.com>
2022-11-26 09:19:12 +0530

    Mysql Script

--- mysql.sh
+++ mysql.sh
@@ -11,12 +11,12 @@ if [ $? -ne 0]
   then
 #resetting of password
 
-  echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Roboshop@1';" >/tmp/root-pass-sql
-  ##(here above cmnd we have entered the password but that not ac ode standards always don't hard code the passwords)
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Roboshop@1';" >/tmp/root-pass-sql
+    ##(here above cmnd we have entered the password but that not ac ode standards always don't hard code the passwords)
 
 
-  DEFAULT_PASSWORD=$(grep 'A temporary password is generated' /var/log/mysqld.log | awk '{print $NF}')
+    DEFAULT_PASSWORD=$(grep 'A temporary password is generated' /var/log/mysqld.log | awk '{print $NF}')
 
 
-  cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
+    cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
 fi
