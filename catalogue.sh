2b46dca706edcfcacac9f3e49a3c931ec6743a78
Venu <vennugopal.challa369@gmail.com>
2022-11-27 21:14:35 +0530

    Adding NODEJS Function

--- catalogue.sh
+++ catalogue.sh
@@ -1,24 +1,31 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
+COMPONENT=catalogue
+source common.sh
 
-useradd roboshop
+NODEJS
 
-
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-cd /home/roboshop
-rm -rf catalogue
-unzip /tmp/catalogue.zip
-mv catalogue-main catalogue
-cd /home/roboshop/catalogue
-npm install
-
-sed -i -e 's/MONGO_DNSNAME/mongod.devops69.onlnine/' systemd.service
-
-
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl start catalogue
-systemctl enable catalogue
+#PRINT
+#
+#curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+#yum install nodejs -y
+#
+#useradd roboshop
+#
+#
+#curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+#cd /home/roboshop
+#rm -rf catalogue
+#unzip /tmp/catalogue.zip
+#mv catalogue-main catalogue
+#cd /home/roboshop/catalogue
+#npm install
+#
+#sed -i -e 's/MONGO_DNSNAME/mongod.devops69.onlnine/' systemd.service
+#
+#
+#mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+#systemctl daemon-reload
+#systemctl start catalogue
+#systemctl enable catalogue
 
 
 
