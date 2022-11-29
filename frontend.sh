7bd2c6f1496f26565a53a3edc0ca53c142f01f68
Venu <vennugopal.challa369@gmail.com>
2022-11-29 19:29:25 +0530

    Updataing roboshops config

--- frontend.sh
+++ frontend.sh
@@ -16,6 +16,11 @@ PRINT "Copy Roboshop Configuration File"
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 STAT $?
 
+PRINT "Update Roboshop Configuration"
+sed -i -e '/catalogue/ s/localhost/dev-catalogue.venudevops.online/' -e '/user/ s/localhost/dev-user.venudevops.online/' -e '/cart/ s/localhost/dev-cart.venudevops.online/' -e '/shipping/ s/localhost/dev-shipping.venudevops.online/' -e '/payment/ s/localhost/dev-payment.venudevops.online/' /etc/nginx/default.d/roboshop.conf
+
+STAT $?
+
 PRINT "enable nginx"
 systemctl enable nginx
 STAT $?
